8244d2cc6ba231d540e9b5a75f731fa5d04f621a
Talha <talhakucuk26@gmail.comgit config --global user.name Talhagit commitgit config --global user.email talhakucuk26@gmail.com>
2023-07-27 19:22:35 +0300

    B harfi yazdıran program.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] letterB = new String[7][7];
+
+        for (int i = 0; i < letterB.length; i++) {
+            for (int j = 0; j < letterB[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letterB[i][j] = ".";
+                } else if ((j == 0 || j == 3)) {
+                    letterB[i][j] = ".      ";
+                } else {
+                    letterB[i][j] = " ";
+                }
+            }
+        }
+
+        for (String[] row : letterB){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
